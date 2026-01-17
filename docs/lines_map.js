7728ec6b2dfbd8ce095e0e718e20fe55bfab7fe4
Singh Siddhant Narayan <e1375752@u.nus.edu>
2026-01-17 20:07:20 +0800

    Add iPhone 1 character interface and audio files to docs for GitHub Pages

---
+++ docs/lines_map.js
@@ -0,0 +1,27 @@
+window.AUDIO_ID_MAP = {
+    322: { file: 'assets/audio/collision_2_2.mov', text: 'This is malarkey!' },
+    421: { file: 'assets/audio/stuck_2_1.mp3', text: 'I am overwhelmed! Overwhelmed, overworked, and showing too much mercy!' },
+    920: { file: 'assets/audio/saw_human_2_0.mp3', text: 'You\'re doomed!' },
+    950: { file: 'assets/audio/random_0_0.mp3', text: 'Have you suffered a recent blow to the head?' },
+    960: { file: 'assets/audio/random_1_0.mov', text: 'What sort of background do you have in science?' },
+    970: { file: 'assets/audio/random_2_0.mp3', text: 'Bazinga!' },
+    971: { file: 'assets/audio/random_2_1.mov', text: 'Oh, you want me to share credit? Get out.' },
+};
+
+window.LINES = {
+    "COLLISION": [
+        322
+    ],
+    "STUCK": [
+        421
+    ],
+    "SAW_HUMAN": [
+        920
+    ],
+    "RANDOM": [
+        950,
+        960,
+        970,
+        971
+    ]
+};
